4463deb3c0eb1d785723b6f88ccf83482da16e65
petermunene <peter.munene@student.moringaschool.com>
2025-05-08 12:54:38 +0300

    tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even=[]
+    for num in num_list:
+        if(num%2==0):
+            even.append(num)
+    return even
     pass
 
 def make_exclamation(sentence_list):
+    lines=[]
+    for sentence in sentence_list:
+        lines.append(sentence + "!")
+    return lines
     pass
